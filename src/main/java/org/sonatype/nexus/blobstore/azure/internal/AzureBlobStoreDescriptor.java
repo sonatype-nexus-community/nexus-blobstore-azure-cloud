9db5608af46c767f15b8dcebbafef04022ab4312
Matt Johnson <mjohnson@sonatype.com>
2018-12-13 11:34:01 -0500

    Adds required configuration fields to descriptor.

--- src/main/java/org/sonatype/nexus/blobstore/azure/internal/AzureBlobStoreDescriptor.java
+++ src/main/java/org/sonatype/nexus/blobstore/azure/internal/AzureBlobStoreDescriptor.java
@@ -12,7 +12,6 @@
  */
 package org.sonatype.nexus.blobstore.azure.internal;
 
-import java.util.Collections;
 import java.util.List;
 
 import javax.inject.Named;
@@ -21,6 +20,10 @@ import org.sonatype.goodies.i18n.I18N;
 import org.sonatype.goodies.i18n.MessageBundle;
 import org.sonatype.nexus.blobstore.BlobStoreDescriptor;
 import org.sonatype.nexus.formfields.FormField;
+import org.sonatype.nexus.formfields.PasswordFormField;
+import org.sonatype.nexus.formfields.StringTextFormField;
+
+import static java.util.Arrays.asList;
 
 @Named(AzureBlobStore.TYPE)
 public class AzureBlobStoreDescriptor
@@ -32,11 +35,47 @@ public class AzureBlobStoreDescriptor
     @DefaultMessage("Azure Cloud Storage")
     String name();
 
+    @DefaultMessage("Account Name")
+    String accountNameLabel();
+
+    @DefaultMessage("Account name found under Access keys for the storage account.")
+    String accountNameHelp();
+
+    @DefaultMessage("Account Key")
+    String accountKeyLabel();
+
+    @DefaultMessage("Account key found under Access keys for the storage account.")
+    String accountKeyHelp();
+
+    @DefaultMessage("Container Name")
+    String containerNameLabel();
+
+    @DefaultMessage("The name of an existing container to be used for storage.")
+    String containerNameHelp();
+
   }
 
   private static final Messages messages = I18N.create(Messages.class);
 
+  private final FormField accountName;
+
+  private final FormField accountKey;
+
+  private final FormField containerName;
+
   public AzureBlobStoreDescriptor() {
+    this.accountName = new StringTextFormField(AzureBlobStore.ACCOUNT_NAME_KEY,
+        messages.accountNameLabel(),
+        messages.accountNameHelp(),
+        true);
+    this.accountKey = new PasswordFormField(AzureBlobStore.ACCOUNT_KEY_KEY,
+        messages.accountKeyLabel(),
+        messages.accountKeyHelp(),
+        true);
+    this.containerName = new StringTextFormField(AzureBlobStore.CONTAINER_NAME_KEY,
+        messages.containerNameLabel(),
+        messages.containerNameHelp(),
+        true);
   }
 
   @Override
@@ -46,6 +85,6 @@ public class AzureBlobStoreDescriptor
 
   @Override
   public List<FormField> getFormFields() {
-    return Collections.emptyList();
+    return asList(accountName, accountKey, containerName);
   }
 }
